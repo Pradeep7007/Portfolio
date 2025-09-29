2f4c101ba76f08e818bfcdeee6000def638dbbfc
Pradeep7007 <pmspradeep007@gmail.com>
2025-09-29 11:53:32 +0530

    skills images added

--- src/assests/Deatils.js
+++ src/assests/Deatils.js
@@ -2,6 +2,17 @@ import java from './java_basic.png';
 import cisco from './cisco.png';
 import sql from './sql_basic.png';
 import codechef from './codechef.png';
+import c from './c program.png';
+import java_logo from './java.png';
+import python from './python.png';
+import html from './html.jpg';
+import css from './css.png';
+import javascript from './javascript.jpg';
+import node from './node.png';
+import express from './express.png';
+import figma from './figma.jpg';
+import bootstrap from './bootstrap.png';
+import github from './github.webp';
 
 export const certifications = [
   {
@@ -23,5 +34,42 @@ export const certifications = [
     title: "CodeChef Problem Solving",
     img: codechef,
     explanation: "Active participant on CodeChef platform with a focus on problem-solving and competitive programming.",
+  },
+];
+
+
+
+export const my_stack = [
+  {
+    category: "Programming Languages",
+    skills: [
+      { name: "C", image: c },
+      { name: "Java", image: java_logo },
+      { name: "Python", image: python },
+      { name: "JavaScript", image: javascript },
+    ],
+  },
+  {
+    category: "Frontend",
+    skills: [
+      { name: "HTML", image: html },
+      { name: "CSS", image: css },
+      { name: "Bootstrap", image: bootstrap },
+      { name: "JavaScript", image: javascript },
+    ],
+  },
+  {
+    category: "Backend",
+    skills: [
+      { name: "Node.js", image: node },
+      { name: "Express.js", image: express },
+    ],
+  },
+  {
+    category: "Tools & Design",
+    skills: [
+      { name: "Figma", image: figma },
+      { name: "GitHub", image: github },
+    ],
   },
 ];
